15a6f07e648927584cddb913fc403efce95c224d
Lucas Ramon <lucas.or.ramon@gmail.com>
2023-04-22 18:08:37 -0300

    feat: add scenario treatments

--- src/main/java/br/com/devcanoa/finance/api/domain/model/FinanceDate.java
+++ src/main/java/br/com/devcanoa/finance/api/domain/model/FinanceDate.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 public record FinanceDate(int year, int month) {
 
     public static FinanceDate fromNumericValue(int numericValue) {
+        if (numericValue < 1) {
+            throw new IllegalArgumentException("numericValue must be greater than zero");
+        }
         final int year = ((numericValue - 1) / 12) + 1;
         final int month = ((numericValue - 1) % 12) + 1;
         return new FinanceDate(year, month);
@@ -16,16 +19,25 @@ public record FinanceDate(int year, int month) {
     }
 
     public FinanceDate minusMonths(final int i) {
+        if (i < 0 || i > 11) {
+            throw new IllegalArgumentException("i must be between 0 and 11");
+        }
         return (month - i < 1)
                 ? new FinanceDate(year - 1, 12 - (i - month))
                 : new FinanceDate(year, month - i);
     }
 
     public int monthsBetween(final FinanceDate end) {
+        if (end.getNumericValue() < getNumericValue()) {
+            throw new IllegalArgumentException("end must be greater than this");
+        }
         return (end.year() - year) * 12 + (end.month() - month) + 1;
     }
 
     public int getNumericValue() {
+        if (year < 1 || month < 1 || month > 12) {
+            throw new IllegalArgumentException("year and month must be greater than zero");
+        }
         return ((year - 1) * 12) + month;
     }
 
